e21a5358032252c84a9100040863d2df70d1ddc5
fedorum <isaaccheah@outlook.com>
2025-06-17 17:37:34 +0800

    Added button functionalities to retrieve user input for theme and timer

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// testing
+// generating quotes based on user-input
+function getTheme() {
+    var input = document.getElementById("theme");
+    var theme = input.value;
+    console.log(theme);
+    input.value = "";
+}
+
+document.getElementById("generateCustom").addEventListener("click", getTheme);
+
+    // AI generation
+
+// implement timer functionality
+function getTimer() {
+    var input = document.getElementById("timer");
+    var timer = input.value;
+    console.log(timer);
+    input.value = "";
+}
+
+document.getElementById("startTimer").addEventListener("click", getTimer);
+
+// // all purpose function for getting user input in field after clicking button (FIX)
+// function getInput(field) {
+//     var input = document.getElementById(field);
+//     var value = input.value;
+//     console.log(value);
+//     input.value = "";
+// }

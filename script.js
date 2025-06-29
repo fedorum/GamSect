53dfbe0cb351773c1929427e91a8451b11ed7127
fedorum <isaaccheah@outlook.com>
2025-06-29 23:58:17 +0800

    Added minutes and seconds separation for the timer function

--- script.js
+++ script.js
@@ -12,25 +12,41 @@ document.getElementById("generateCustom").addEventListener("click", getTheme);
 
 // timer functionality
 var interval;
-var timer = document.getElementById("userTime")
+var time = document.getElementById("userTime") * 60;
+var minutes;
 var seconds;
 
 function setTimer() {
+    time = userTime.value * 60;
     interval = setInterval(startTimer, 1000);
-    seconds = timer.value;
-    document.getElementById("timer").innerHTML = "Time: " + seconds;
+    minutes = Math.floor(time / 60);
+    seconds = time % 60;
+    document.getElementById("timer").innerHTML = "Time: " + minutes + ":" + seconds;
     document.getElementById("userTime").value = "";
     }
 
 document.getElementById("startTimer").addEventListener("click", setTimer);
 
 function startTimer() {
-    seconds -= 1;
-    document.getElementById("timer").innerHTML = "Time: " + seconds;
-    if (seconds < 0) {
+    time -= 1;
+    minutes = Math.floor(time / 60);
+    seconds = time % 60;
+    document.getElementById("timer").innerHTML = "Time: " + minutes + ":" + seconds;
+
+    if (time < 0) {
         clearInterval(interval)
         document.getElementById("timer").innerHTML = "Time: 00:00";
     }
 }
+
+// function displayTime() {
+//     minutes = Math.floor(time / 60);
+//     seconds = time % 60;
+//     if (minutes == 0 && minutes <= 9) {
+//         minutes 
+//     }
+
+//     document.getElementById("timer").innerHTML = "Time: " + minutes + ":" + seconds;
+// }
 
 // resetting timer function when new user input given

5147c20c007518c011ac17d7a984ec0072036170
fedorum <isaaccheah@outlook.com>
2025-09-22 09:40:46 +0800

    Committed gemini script for cross development

---
+++ src/gemini.js
@@ -0,0 +1,17 @@
+// PROMPT GENERATION
+
+// importing the gemini module for the prompt generation
+import { GoogleGenAI } from "@google/genai";
+
+// const apiKey = process.env.GEMINI_API_KEY;
+// const ai = new GoogleGenAI({ apiKey: apiKey});
+
+// generates prompts based on the user-inputted theme in index.js
+export async function generatePrompts(theme) {
+    // const response = await ai.models.generateContent({
+    //     model: "gemini-2.5-flash",
+    //     contents: "Explain how AI works in a few words",
+    // });
+    console.log(theme);
+    // return prompts generated from json?
+}
